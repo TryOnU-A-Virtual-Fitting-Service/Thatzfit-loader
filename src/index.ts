8d27fe44add828ead54a6a4ee6abf2f886d867bb
lee0jae330 <xingxing2001@cau.ac.kr>
2025-11-22 16:12:34 +0900

    refactor: clean up and improve Thatzfit SDK integration logic

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,94 @@
 declare global {
-    interface Window {
-        Thatzfit : IThatzfit;
-        ThatzfitInitialized?:boolean;
-    }
+  interface Window {
+    Thatzfit: IThatzfit;
+    ThatzfitInitialized?: boolean;
+  }
 }
 
 interface IThatzfit {
-    command?: (...args: any) => void;
-    queue?: [method: string, ...args:any[]][]
-    (...args: any) : void;
+  command?: (...args: any) => void;
+  queue?: [method: string, ...args: any[]][];
+  (...args: any): void;
 }
 
 const isSSR = () => {
-    if(typeof window === "undefined") {
-        console.error('Thatzfit SDK Loader is not supported in SSR');
-        return true;
-    }
-    return false;
-}
+  if (typeof window === "undefined") {
+    console.error("Thatzfit SDK Loader is not supported in SSR");
+    return true;
+  }
+  return false;
+};
 
 const isSDKLoaded = () => {
-    if(!window.Thatzfit) {
-        console.error('Thatzfit SDK is not loaded');
-        return false;
-    }
-    return true;
-}
-
+  if (!window.Thatzfit) {
+    console.error("Thatzfit SDK is not loaded");
+    return false;
+  }
+  return true;
+};
 
 const safeExecuteThatzfit = (...args: unknown[]) => {
-    if(isSSR() || !isSDKLoaded()) {
-        return;
-    }
-    window.Thatzfit.command?.(...args);
-}
+  if (isSSR() || !isSDKLoaded()) {
+    return;
+  }
+  window.Thatzfit.command?.(...args);
+};
 
 export const loadScript = () => {
-    if(isSSR()) {
-        return;
+  if (isSSR()) {
+    return;
+  }
+
+  (() => {
+    const w = window;
+    if (!!w.Thatzfit) {
+      return;
     }
-    
-    (() => {
-        const w = window;
-        if(!!w.Thatzfit) {
-            return;
-        }
-    
-        const thatzfit : IThatzfit = function() {
-            thatzfit.command?.(...arguments)
-        }
-    
-        thatzfit.queue = [];
-        thatzfit.command = (args) => {
-            thatzfit.queue?.push(args);
-        }
-        w.Thatzfit = thatzfit;
-    
-        const load = () => {
-            if(w.ThatzfitInitialized) {
-                return;
-            }
-            w.ThatzfitInitialized = true;
-    
-            const s= document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src= "https://cdn.thatzfit.com/plugin/ThatzfitSDKInjector.js";
-    
-            const firstScript = document.getElementsByTagName('script')[0];
-            if(firstScript.parentNode) {
-                firstScript.parentNode.insertBefore(s, firstScript);
-            }
-        }
-    
-        if(document.readyState === 'complete') {
-            load();
-        } else {
-            w.addEventListener('DOMContentLoaded', load);
-            w.addEventListener('load', load);
-        }
-    })()
-}
+
+    const thatzfit: IThatzfit = function () {
+      thatzfit.command?.(...arguments);
+    };
+
+    thatzfit.queue = [];
+    thatzfit.command = (args) => {
+      thatzfit.queue?.push(args);
+    };
+    w.Thatzfit = thatzfit;
+
+    const load = () => {
+      if (w.ThatzfitInitialized) {
+        return;
+      }
+      w.ThatzfitInitialized = true;
+
+      const s = document.createElement("script");
+      s.type = "text/javascript";
+      s.defer = true;
+      s.src = "https://cdn.thatzfit.com/plugin/ThatzfitSDKInjector.js";
+
+      const firstScript = document.getElementsByTagName("script")[0];
+      if (firstScript.parentNode) {
+        firstScript.parentNode.insertBefore(s, firstScript);
+      }
+    };
+
+    if (document.readyState === "complete") {
+      load();
+    } else {
+      w.addEventListener("DOMContentLoaded", load);
+      w.addEventListener("load", load);
+    }
+  })();
+};
 
 export interface BootOption {
-    pluginKey : string;
+  pluginKey: string;
 }
 
 export const boot = (option: BootOption) => {
-    safeExecuteThatzfit('boot', option);
-}
+  safeExecuteThatzfit("boot", option);
+};
 
-export const shutdowㅜ = () => {
-    safeExecuteThatzfit('shutdown');
-}
+export const shutdown = () => {
+  safeExecuteThatzfit("shutdown");
+};
